027bf670e4b832adb0cf3d4a5851ee87d85dfb0a
amoddevkar <87513080+amoddevkar@users.noreply.github.com>
2024-10-08 10:02:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [city, setCity] = useState("")
+  const [temp, setTemp] = useState("")
+  const [humidity, setHumidity] = useState("")
+  const [condition, setCondition] = useState("")
+  const [windspeed, setWindSpeed] = useState("")
+  const [loading, setLoading] = useState("")
+
+  const handleSubmit = async () => {
+    try {
+      setLoading(true)
+      const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=8ed0f84b16d1451fb5c32057240810&q=${city}`)
+      if (!res.ok) throw new Error("Invalid data")
+      console.log(res)
+      const data = await res.json()
+      setTemp(data.current.temp_c)
+      setHumidity(data.current.humidity)
+      setCondition(data.current.condition.text)
+      setWindSpeed(data.current.wind_kph)
+      setLoading(false)
+    } catch (error) {
+      alert("Failed to fetch weather data")
+      setLoading(false)
+    }
+  }
+  const handleInputChange = (e) => {
+    setTemp("")
+    setHumidity("")
+    setCondition("")
+    setWindSpeed("")
+    setCity(e.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={handleInputChange} />
+      <button onClick={handleSubmit}>Search</button>
+      {temp && humidity && condition && windspeed &&
+        <div className="weather-cards">
+          <div className="weather-card"><h2>Temperature</h2><p>{temp}</p></div>
+          <div className="weather-card"> <h2>Humidity</h2><p>{humidity}</p></div>
+          <div className="weather-card"> <h2>Condition</h2><p>{condition}</p></div>
+          <div className="weather-card"><h2>Wind Speed</h2><p>{windspeed}</p></div>
+        </div>}
+      {loading && <p>Loading data...</p>}
     </div>
   );
 }
